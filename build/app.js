290a09a74a49453d2a79d193dfc3d36d61248172
Seungchan Lee <seunggs@gmail.com>
2016-01-20 11:35:05 -0800

    complete setup

---
+++ build/app.js
@@ -0,0 +1,8 @@
+webpackJsonp([0,2],[
+/* 0 */
+/***/ function(module, exports) {
+
+	eval("\"use strict\";//@ sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIwLmpzIiwic291cmNlUm9vdCI6IiJ9");
+
+/***/ }
+]);
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+var path = require('path');
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
+
+// definePlugin takes raw strings and inserts them, so you can put strings of JS if you want.
+var definePlugin = new webpack.DefinePlugin({
+  __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
+  __PRERELEASE__: JSON.stringify(JSON.parse(process.env.BUILD_PRERELEASE || 'false'))
+});
+
+var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
+
+module.exports = {
+	entry: {
+		public: path.resolve(__dirname, 'public', 'main.js'),
+		app: path.resolve(__dirname, 'app', 'main.js')
+	},
+	output: {
+		path: path.resolve(__dirname, 'build'),
+    publicPath: '/build/',
+		filename: '[name].js'
+	},
+	devServer: {
+    host: '127.0.0.1',
+		port: 3333
+	},
+  devtool: 'eval-source-map',
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: {
+					presets: ['es2015', 'react']
+				}
+			},
+			{
+        test:   /\.css$/,
+        loader: "style-loader!css-loader!postcss-loader"
+      },
+			{
+				test: /\.(png|jpg|gif|svg|woff|ttf|otf|eot)$/,
+				loader: 'url-loader?limit=8192'
+			}
+		]
+	},
+	postcss: function () {
+    return [autoprefixer, precss];
+  },
+	resolve: {
+    extensions: ['', '.js', '.json']
+  },
+  plugins: [definePlugin, commonsPlugin]
+}
